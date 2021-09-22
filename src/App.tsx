88f9e99f11b816f4ea8ff25ade5b34957224f097
Steven Dostert <s.dostert@weframe.com>
2021-09-22 17:14:50 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import { KonvaComp } from "./konva/konva";
 import { PixiComp } from "./pixi/pixi";
 
+export type NoteType = {
+  x: number;
+  y: number;
+  size: number;
+  content: string;
+};
+
 export const SIZE = {
   width: 3840,
   height: 2160,
 };
 
-export const artifacts = Array(100).fill({
+export const artifacts: NoteType[] = Array(100).fill({
   x: 0,
   y: 0,
   size: 100,
@@ -19,7 +26,9 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => setIsPixi(!isPixi)} />
+      <button onClick={() => setIsPixi(!isPixi)}>
+        Using: {isPixi ? "Pixi" : "Konva"} (press to toggle)
+      </button>
       {isPixi ? <PixiComp /> : <KonvaComp />}
     </div>
   );
